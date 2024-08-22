2f69b5082c09bbb57e4fc01a98643e5279e889a9
Max Konin <maxim21214@gmail.com>
2024-08-21 22:57:18 -0300

    Fix tests and linter

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -4,6 +4,6 @@ module.exports = {
   extends: ['@repo/eslint-config/library.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.lint.json',
+    project: './tsconfig.json',
   },
 };
